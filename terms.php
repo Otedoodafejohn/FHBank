936b87d93d10f9dfaa4fd4746a298f782f92bbb5
Otedo Odafe John <jonajohn74@gmail.com>
2024-07-25 19:16:06 +0100

    #30

--- terms.php
+++ terms.php
@@ -4,7 +4,54 @@
 ?>
 <link rel="stylesheet" href="fhbCSS/terms.css">
 <h2>Terms & Conditions</h2>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, assumenda, sequi totam quia libero ullam voluptates ex culpa quas reiciendis esse deserunt facilis non quam, fuga eos unde recusandae corrupti.</p>
+<p>
+    <h3>WELCOME TO FLEX HORIZON BANK!</h3>
+</p>
+<p>
+The Flex Horizon Banking Service, website and/or software application (collectively, the “Service”) enables you to transfer funds between your mobile money wallet and your bank account, as well as to the wallet/account of another user. <br/>
+ It also enables you, through your supported mobile money wallet, account or debit/credit card, to pay for products and services offered by third parties (“Products”).<br/>
+ The Service is operated by Flex Horizon Bank(“FHBank”), subject to these Terms of Service (“Terms”).
+</p>
+<p>
+    <h4>Terms of Services</h4>
+</p>
+<p>
+<b>This is a contract.<b> These Terms constitute a contract between you and FHBank. If you do not accept these Terms, please do not use the Service. By using any part of the Service, you accept these Terms. If you are under eighteen (18) years of age, you must not use the Service.
+
+You must register a wallet account with valid information.To use the Service, you must: (a) provide a valid mobile phone number; (b) agree that an account associated with such number be created; (c) accept the current Terms; and (d) agree to submit such other (and additional) information as FHBank may request in line with regulatory/legal provisions. You agree to provide true, accurate and complete information about yourself as prompted by FHBank during the account registration process. You must ensure that any third-party information you provide to FHBank has been authorized appropriately. You will be given the option of receiving a unique PIN for purposes of reusing your account. You are responsible for keeping your PIN secure at all times, without reservation. For the avoidance of doubt, FHBank will not be responsible when/where you compromise your details by disclosing it to third parties.
+
+<b>Third-party merchants offer the goods & services.<b> The Service provides an opportunity to discover offers from independent third-party merchants (“Merchants”). These Merchants sell their respective Products subject to their own terms and conditions. To illustrate:
+<b>Airtime:<b> If you choose to purchase pre-paid airtime, you do so pursuant to the relevant terms and conditions of your mobile telecommunications provider.
+<b>Utilities:<b> If you choose to purchase pre-paid electricity or water, you do so pursuant to the relevant terms and conditions of the utility provider.
+
+If you decide to purchase Products such as these from third party Merchants, you do so at your own risk and subject to the relevant Merchant’s terms and conditions. We are unable to investigate, monitor, or check the products for accuracy, reliability, appropriateness or completeness. By granting you access to discover the Products via the Service, FHBank does not imply approval or endorsement of such Products. You therefore agree that FHBank is not responsible or liable for the Products you purchase from Merchants.
+
+Payments are processed by third parties.In order to facilitate your purchases from Merchants, FHBank has integrated its Service with the billing solutions of certain mobile money providers and credit card issuing banks (“Payment Processors”). When you purchase a Product from a Merchant using your supported mobile money wallet or account or debit/credit card, payments will be processed by such Payment Processors. Payment Processors have their own terms and conditions governing your use of their payment services. You understand and agree that OPay does not process payments and is not responsible or liable for any transactions in which you engage. By engaging in a purchase transaction using your mobile money account or debit/credit card, you authorize the Merchant (or FHBank on its behalf) to bill your account or card for the purchase amount.
+
+Your privacy is important to us. FHBank takes the matters of protection and security of its users’ information very seriously. FHBank’s privacy policy governing the Service is attached to these Terms (“Privacy Policy”). The Privacy Policy is incorporated into these Terms by this reference. By using the Service, you agree to the use of your data under FHBanks Privacy Policy. The Privacy Policy addresses only the personal information processed by FHBank in providing the Service to you. Merchants and Payment Processors have their own information-gathering practices which apply when you choose to purchase their Products.
+
+Product inquiries should be submitted to the relevant Merchant.If you have any questions, concerns or requests related to a Product you have purchased from a Merchant, please contact the Merchant directly or contact us at ng-support@opay-inc.com for assistance. Each Merchant maintains its own customer contact desk for handling such requests, however FHBank will do its best to assist you. If you wish to submit a complaint about the practices of a Merchant offering its Products via the Service, you may contact us by email at ng-support@opay-inc.com.
+
+You must not use the Service to violate any laws. You must not use the Service to violate or infringe the rights of any other person, including the rights of other users, FHBank’s rights in the Service or Merchants’ rights in their Products. You must not breach any laws or regulations when using the Service or attempt to disrupt or interfere with the security or functionality of the Service. In the event that FHBank reasonably suspects that you are using the Services for illegal activities (particularly fraud, money laundering or other related illegalities), FHBank reserves the right to block your account immediately without liability. Such illegalities as mentioned above may be as a result of enacted laws, court judgments, rulings or pronouncements, or regulatory policies and guidelines to which FHBank is bound. If a breach is associated with your account/wallet, you agree that we have the right to apply restrictions to your account/wallet and report to the appropriate law enforcement agencies or the respective regulator, in line with extant laws without informing you.
+
+<b>FHBank may contact you regarding your account or the Service.<b> You expressly agree that, as part of the Service, you may, from time to time, receive communications from FHBank via email, instant message, telephone, text message (SMS), emails or other means. You may elect to stop receiving promotional messages by emailing your request to opt-out, along with your cell phone number, to ng-support@opay-inc.com, or following the opt-out instructions in the message. Even if you choose to opt out of receiving promotional messages, you may not opt out of receiving service-related messages as these ensure that we are able to deliver accurate, relevant, sensitive and security-related services to you.
+
+<b>FHBank may discontinue the Service.<b> FHBank may, in furtherance of regulatory, time-sensitive and security-related purposes, terminate your access to the Service or discontinue providing the Service or any part of the Service, with due notice to you [or without notice where the suspension or termination is expedient to forestall, curb or extinguish some ongoing fraud, industry-wide compromise or an ongoing financial crime-related investigation]. Rest assured that we will provide as much notice as the circumstance allows, and restore the Service at the earliest convenience. You agree that in the event of the foregoing, FHBank will not be responsible or liable to you or any third party.
+
+<b> FHBank is not liable for any damages you may incur as a result of using the Services.<b> In no event shall FHBank or its suppliers be liable for any direct, indirect, punitive, incidental, special, consequential damages, or any damages whatsoever arising out of, or in any way connected with the use or performance of the service, with the delay or inability to use the service, the provision of (or failure to provide services), or otherwise arising out of the use of the service, whether based on contract, tort, negligence, strict liability, or otherwise, even if FHBank or any of its suppliers have been advised of the possibility of such damages.
+
+You agree to mediate any disputes.This Agreement is subject to, and shall be governed by, and construed in accordance with the laws of Nigeria, without reference to the principles of conflict of laws thereof. Any matters arising concerning the interpretation, validity or implementation of this Agreement not solved by mutual agreement between the Parties shall be submitted to mediation in the English language before a sole mediator to take place in Lagos, Nigeria, unless impractical to do so. Nothing in this Agreement will be deemed as preventing FHBank from seeking injunctive relief (or any other provisional remedy) from any court having jurisdiction over the Parties and the subject matter of the dispute as is necessary to protect FHBank’s name, proprietary information, trade secrets, know-how, or any other intellectual property rights.
+
+<b>FHBank may modify these Terms.<b> We may modify these Terms and related policies (including, but not limited to the Privacy Policy) at any time in the future. Where this happens, we will communicate the changes to you. By continuing to use the Service, you agree to be bound by the latest version of these Terms. It is your responsibility to familiarize yourself with the communicated changes.
+
+<b>Alteration.<b> No alteration, variation or agreed cancellation of this agreement, and the Privacy Policy, shall be of any effect unless so directed by us.
+
+<b>Whole Agreement.<b> This Agreement constitutes the whole agreement between the parties in regard to the subject matter hereof and no warranties or representations of any nature whatsoever other than set out in this agreement have been given by any of the parties.
+
+<b>Waiver/Relaxation.<b> No relaxation or indulgence which OPay may show to you shall in any way prejudice or be deemed to be a waiver of its rights hereunder.
+
+<b>Survival.<b> Each and every provision of this Agreement (excluding only those provisions which are essential at law for a valid and binding Agreement to be constituted) shall be deemed to be separate and severable from the remaining provisions of this Agreement. If any of the provisions of this Agreement (excluding only those provisions which are essential at law for a valid and binding Agreement to be constituted) is found by any court of competent jurisdiction to be invalid and/or unenforceable then, notwithstanding such invalidity and/or unenforceability, the remaining provisions of this Agreement shall be (and remain) of full force and effect.
+</p>
 <?php
     require_once("assets/footer.php");
 ?>
